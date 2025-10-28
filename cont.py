c4320becbe3601bab53a89af2824b586d1a22fea
miracvitus19-svg <miracvitus19@gmail.com>
2025-10-28 21:36:26 +0300

    Update cont.py

--- cont.py
+++ cont.py
@@ -10,7 +10,7 @@ TURKISH_TZ = ZoneInfo("Europe/Istanbul")
 
 # ----------------------------- Ayarlar ve Başlık -----------------------------
 st.set_page_config(
-    page_title="Zamanlı Görsel Şifreleme Uygulaması",
+    page_title="Zamanlı Görsel Şifreleme",
     page_icon="🖼️",
     layout="wide"
 )
